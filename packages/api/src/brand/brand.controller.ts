b5c0a01858e55240ad1c0246dcced2a001154d1d
Vladimir Zaychikov <zaychikov.vl@gmail.com>
2021-07-19 23:37:52 +0300

    бренд работает

--- packages/api/src/brand/brand.controller.ts
+++ packages/api/src/brand/brand.controller.ts
@@ -9,8 +9,20 @@ import {
     ParseIntPipe,
     Body
 } from "@nestjs/common";
+import { BrandService } from "@brand/brand.service";
+import { BrandDto } from '@brand/dto'
 
 @Controller('brand')
 export class BrandController {
+    constructor(private readonly brandService: BrandService) {}
 
+    @Put('create')
+    create(@Body() brand: BrandDto) {
+        return this.brandService.create(brand);
+    }
+
+    @Get()
+    getList() {
+        return this.brandService.getList();
+    }
 }
--- packages/api/src/brand/entities/brand.entity.ts
+++ packages/api/src/brand/entities/brand.entity.ts
@@ -9,6 +9,21 @@ export class Brand {
     @PrimaryGeneratedColumn("uuid")
     id!: string;
 
+    @Column({ type: "varchar", length: 30 })
+    type!: string;
+
     @Column({ type: "varchar", length: 40 })
-    companyName!: string;
+    name!: string;
+
+    @Column({ type: "varchar", length: 100 })
+    brandNames!: string;
+
+    @Column({ type: "integer" })
+    shopsTotal!: number;
+
+    @Column({ type: "integer" })
+    shopsInMalls!: number;
+
+    @Column({ type: "varchar", length: 40 })
+    malls!: string;
 }
